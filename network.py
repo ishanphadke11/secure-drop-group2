dcc9ce17c4044c3df2823bd2c74bca8a90af15d2
Jennifer Allen <jennifer.l.allen27@gmail.com>
2026-04-24 14:00:28 -0400

    Update network.py

--- network.py
+++ network.py
@@ -8,34 +8,34 @@ ONLINE_USERS = []
 PORT = 50000
 
 def start_listender():
-  def listen():
-    global ONLINE_USERS
-    s = socket.socket(socket,AF_INET, socket.SOCK_DGRAM)
-    s.bind(('', PORT))
+    def listen():
+        global ONLINE_USERS
+        s = socket.socket(socket,AF_INET, socket.SOCK_DGRAM)
+        s.bind(('', PORT))
 
-while True:
-  data, addr = s.recvfrom(4096)
-  try:
-    user = json.loads(data.decode())
-    if user not in ONLINE_USERS:
-      ONLINE_USERS.append(user)
-  except:
-    continue
+        while True:
+            data, addr = s.recvfrom(4096)
+            try:
+                user = json.loads(data.decode())
+                if user not in ONLINE_USERS:
+                    ONLINE_USERS.append(user)
+            except:
+              continue
 
-thread = threading.Thread(target=listen, daemon=True)
-thread.start()
+    thread = threading.Thread(target=listen, daemon=True)
+    thread.start()
 
 def broadcast_presence(session, contacts():
-# still need to implement
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# message
-# send message
+    # still need to implement
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # message
+    # send message
 
 def discover_users(session, contacts):
-  global ONLINE_USERS
-  ONLINE_USERS = []
-  broadcast_presence(session, contacts)
-  # wait for a response
-  time.sleep(2)
-  return ONLINE_USERS
+    global ONLINE_USERS
+    ONLINE_USERS = []
+    broadcast_presence(session, contacts)
+    # wait for a response
+    time.sleep(2)
+    return ONLINE_USERS
