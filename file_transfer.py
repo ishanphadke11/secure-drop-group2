b3f2eef5db28b39103b112cf07c54028b06d3a6a
Chan Heng <chan_heng@student.uml.edu>
2026-04-26 03:55:24 +0000

    complete milestone 5

--- file_transfer.py
+++ file_transfer.py
@@ -80,6 +80,7 @@ RECEIVED_FILES = {}
 # receives incoming packets and detects when a full file finished sendinh
 def start_file_receiver():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(("", FILE_PORT))
 
     while True:
